378e6e985e829e5b4f02ffa0dc63f6e172a49b35
pavit1809 <pavitragoyal@hotmail.com>
2021-03-21 07:04:34 +0530

    FINAL COMMIT 4.0

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -3,16 +3,20 @@ const cors=require('cors');
 const path=require('path');
 const bodyParser=require('body-parser');
 const http=require('http');
+const cron=require('node-cron');
 
 require('./db/mongoose');
 
 const app=express();
 const server=http.createServer(app);
+const Rfp=require('./models/rfp');
+const Bid=require('./models/Bid');
 
 const port=process.env.PORT || 5000;
 
 const manufacturerRouter=require('./routers/manufacturer');
 const vendorRouter=require('./routers/vendor');
+const helper=require('./helpers/helper');
 
 app.use(bodyParser.json({limit: '10mb', extended: true}))
 app.use(bodyParser.urlencoded({limit: '10mb', extended: true}))
@@ -29,3 +33,20 @@ app.get('/',(req,res)=>{
 server.listen(port,()=>{
       console.log('Server is running on port:',port);
 }) 
+
+const task=cron.schedule('0 1 * * *',async ()=>{
+      const allRfps=await Rfp.find({});
+      let toremove=[];
+      for(let i=0;i<allRfps.length;i++){
+            if (helper.comparedatecurr(allRfps[i].DeadlineDate)==1){
+                  toremove.push(allRfps[i]._id);
+            }
+      }
+      for(let i=0;i<toremove.length;i++){
+            await Bid.deleteMany({rfp_id:toremove[i]});
+      }
+},{
+      scheduled:false,
+      timezone:'Asia/Kolkata'
+});
+task.start();
